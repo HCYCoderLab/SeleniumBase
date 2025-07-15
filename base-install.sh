09af49c1b5446812f9b6f8872c287af27bd55c6d
HCYCoderLab <cheneyhancn@gmail.com>
2025-07-16 01:22:29 +0800

    Create base-install.sh

---
+++ base-install.sh
@@ -0,0 +1,8 @@
+# 更新依赖
+sudo apt update
+# 下载Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# 安装Chrome
+sudo apt install ./google-chrome-stable_current_amd64.deb
+# 下载seleniumbase
+pip install seleniumbase
